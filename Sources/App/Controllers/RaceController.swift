b589c44f406b503820e4aa561538fca21809e3b3
aeremic <andrija.eremic@gmail.com>
2024-10-29 23:32:31 +0100

    sort schedules in memory (temporary)

--- Sources/App/Controllers/RaceController.swift
+++ Sources/App/Controllers/RaceController.swift
@@ -1,50 +1,59 @@
 import Fluent
+import FluentSQL
 import Vapor
 
 struct RaceController: RouteCollection {
-  func boot(routes: RoutesBuilder) throws {
-    let races = routes.grouped("races")
+    func boot(routes: RoutesBuilder) throws {
+        let races = routes.grouped("races")
 
-    races.get(use: self.index)
-    // races.post(use: self.create)
-    races.group(":id") { race in
-      race.get(use: self.get)
-    }
-  }
-
-  @Sendable
-  func index(req: Request) async throws -> [Race] {
-    let races: [Race] = try await Race.query(on: req.db)
-      .with(\.$schedules)
-      .all()
-
-    return races
-  }
-
-  @Sendable
-  func get(req: Request) async throws -> Race {
-    guard let race = try await Race.find(req.parameters.get("id"), on: req.db)
-    else {
-      throw Abort(.notFound)
+        races.get(use: self.index)
+        // races.post(use: self.create)
+        races.group(":id") { race in
+            race.get(use: self.get)
+        }
     }
 
-    return race
-  }
+    @Sendable
+    func index(req: Request) async throws -> [Race] {
+        let races: [Race] = try await Race.query(on: req.db)
+            .with(\.$schedules)
+            .sort(Race.self, \.$date)
+            .all()
 
-  @Sendable
-  func create(req: Request) async throws -> Race {
-    let race = try req.content.decode(Race.self)
+        // TODO: Sort in db instead of memory sort.
+        for race in races {
+            race.schedules = race.schedules.sorted { schedule1, schedule2 in
+                schedule1.date > schedule2.date
+            }
+        }
 
-    // race.date = Date()
-    // let schedules = [
-    //   Schedule(title: "schedule1", date: Date()),
-    //   Schedule(title: "schedule2", date: Date()),
-    // ]
+        return races
+    }
 
-    try await race.save(on: req.db)
+    @Sendable
+    func get(req: Request) async throws -> Race {
+        guard let race = try await Race.find(req.parameters.get("id"), on: req.db)
+        else {
+            throw Abort(.notFound)
+        }
 
-    // try await race.$schedules.create(schedules, on: req.db)
+        return race
+    }
 
-    return race
-  }
+    @Sendable
+    func create(req: Request) async throws -> Race {
+        let race = try req.content.decode(Race.self)
+
+        // race.date = Date()
+        // let schedules = [
+        //   Schedule(title: "schedule1", date: Date()),
+        //   Schedule(title: "schedule2", date: Date()),
+        // ]
+
+        try await race.save(on: req.db)
+
+        // try await race.$schedules.create(schedules, on: req.db)
+
+        return race
+    }
 }
